001c30ed125f34e52381e96513fae641755ec80a
Nick Pirolo <nicholas.pirolo@gmail.com>
2023-08-16 19:10:54 -0400

    added js to Player and Team components

---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import Team from './Team'
+
+const Player = ({ player }) => {
+  return (
+    <div className="player-card">
+      <h2 className="player-name">{stats.player}</h2>
+      <img src={state.playerImage}></img>
+      <h5 className="player-position">{stats.position}</h5>
+
+      <div className="team">
+        {stats.team.map((team) => (
+          <Team key={team.id} team={team} />
+        ))}
+      </div>
+      <div className="pitching_stats">
+        {movie.genres.map((genre) => (
+          <Genre key={genre.id} genre={genre} />
+        ))}
+      </div>
+      <div className="batting_stats">
+        {movie.genres.map((genre) => (
+          <Genre key={genre.id} genre={genre} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Player
---
+++ src/components/Team.jsx
@@ -0,0 +1,5 @@
+const Team = ({ team }) => {
+  return <div className="team">{stats.team}</div>
+}
+
+export default Team
